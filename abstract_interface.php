15877edc44f36896c133c1e85231b164c93a78b8
Jose Carlos de Jesus Montero Rodriguez <jmonteror@hmujer.saludtab.gob.mx>
2018-07-18 08:33:36 -0500

    Se ha modificado este archivo

--- abstract_interface.php
+++ abstract_interface.php
@@ -1,5 +1,23 @@
 <?php
 
-class Perro {
-    
+class Animal {
+    abstract public function makeSound();
+
+    public function run()
+    {
+
+    }
 }
+
+class Dog{}
+
+class Cat{}
+
+class iDB
+{
+    public function connect();
+}
+
+class MySQL{}
+
+class Oracle{}
